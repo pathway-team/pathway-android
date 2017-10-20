e77b6b3f6c2e576a82761797f1f8c19395b62c05
grif145 <dkcregan@uncg.edu>
2017-10-19 21:42:45 -0400

    started working on and have almost finished the nav drawer, working on fixing an error with the replace action on it

---
+++ Pathway-Android/app/src/main/java/com/pathway/pathway_android/Accountpage.java
@@ -0,0 +1,21 @@
+package com.pathway.pathway_android;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Daniel on 10/18/2017.
+ */
+
+public class Accountpage extends Fragment {
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.account_layout, container, false);
+        return myView;
+    }
+}

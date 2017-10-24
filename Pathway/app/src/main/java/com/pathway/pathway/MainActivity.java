d014699e39af342ececf276ae65dc82f42408c08
Cory Sabol <corygrandhamsabol@gmail.com>
2017-10-23 22:04:03 -0400

    add routes map entry to nav drawer

--- Pathway/app/src/main/java/com/pathway/pathway/MainActivity.java
+++ Pathway/app/src/main/java/com/pathway/pathway/MainActivity.java
@@ -92,7 +92,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_routes_map) {
+            // handle the routes map fragment
+        } else if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 

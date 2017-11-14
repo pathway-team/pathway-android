294ab6da222c11847b2ab3b06bef0d51a7b1e214
Eric West <bactax10@gmail.com>
2017-11-14 16:56:22 -0500

    Fixed dbHanlder issues

--- Pathway/app/src/main/java/com/pathway/pathway/DeviceDBHandler.java
+++ Pathway/app/src/main/java/com/pathway/pathway/DeviceDBHandler.java
@@ -43,6 +43,8 @@ public class DeviceDBHandler extends SQLiteOpenHelper {
     private static final String KEY_NUM_RUNS = "Number_Runs";
 
     //achievements information
+    private static final String KEY_ACH_SET = "isSet";
+    private static final String KEY_ACH_NAME = "Name";
 
     public DeviceDBHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -64,8 +66,8 @@ public class DeviceDBHandler extends SQLiteOpenHelper {
         //creates routes table
         String CREATE_ROUTES_TABLE = String.format("CREATE TABLE IF NOT EXISTS %s " +
                 "(%s INTEGER PRIMARY " + "KEY AUTOINCREMENT, " +
-                "%s INTEGER" +
-                "%s INTEGER" +
+                "%s INTEGER," +
+                "%s INTEGER," +
                 "%s TEXT);", TABLE_ROUTES, KEY_ID, KEY_PID, KEY_RID, KEY_JSON);
 
         db.execSQL(CREATE_ROUTES_TABLE);
@@ -73,26 +75,31 @@ public class DeviceDBHandler extends SQLiteOpenHelper {
         //creates reports table
         String CREATE_report_TABLE = String.format("CREATE TABLE IF NOT EXISTS %s " +
                 "(%s INTEGER PRIMARY " + "KEY AUTOINCREMENT, " +
-                " %s TEXT, " +
-                "%s TEXT);", TABLE_REPORTS, KEY_ID, KEY_JSON);
+                "%s INTEGER," +
+                "%s INTEGER," +
+                " %s TEXT" +
+                ");", TABLE_REPORTS, KEY_ID, KEY_PID, KEY_RID,  KEY_JSON);
         db.execSQL(CREATE_report_TABLE);
 
         //creates user table(holds profile reports)
         String create_tbl_user = String.format("CREATE TABLE IF NOT EXISTS %s " +
                 "(%s INTEGER PRIMARY " + "KEY AUTOINCREMENT, " +
-                "%s REAL" + // total distance
-                "%s REAL" + // total runtime
-                "%s REAL" + // # of routes
-                "%s REAL" + // # of runs
-                ");", TABLE_USER, KEY_ID, KEY_TOT_DIST, KEY_TOT_TIME, KEY_NUM_ROUT, KEY_NUM_RUNS);
+                "%s REAL," + // total distance
+                "%s REAL," + // total runtime
+                "%s REAL," + // # of routes
+                "%s REAL," + // # of runs
+                "%s TEXT" +
+                ");", TABLE_USER, KEY_ID, KEY_TOT_DIST, KEY_TOT_TIME, KEY_NUM_ROUT, KEY_NUM_RUNS, KEY_JSON);
         db.execSQL(create_tbl_user);
 
         //creates achievements table(holds boolean values for when a user qualifies for achievements)
-        /*String create_tbl_achievements = String.format("CREATE TABLE IF NOT EXISTS %s " +
+        String create_tbl_achievements = String.format("CREATE TABLE IF NOT EXISTS %s " +
                 "(%s INTEGER PRIMARY " + "KEY AUTOINCREMENT, " +
-                "%s TEXT);", TABLE_ACHIEVEMENTS, KEY_ID, KEY_JSON);
+                "%s TEXT," +
+                "%s INTEGER" +
+                ");", TABLE_ACHIEVEMENTS, KEY_ID, KEY_ACH_NAME, KEY_ACH_SET);
         db.execSQL(create_tbl_achievements);
-        */
+
     }
 
     @Override
@@ -100,12 +107,49 @@ public class DeviceDBHandler extends SQLiteOpenHelper {
         db.execSQL(String.format("DROP TABLE IF EXISTS %s", TABLE_ROUTES));
         this.onCreate(db);
     }
-
+/*
     public long addRoute(Route path) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
 
         values.put(KEY_JSON, path.toString());
+        values.put(KEY_PID, )
+        values.put(KEY_RID, path.getRID());
+
+        long id = db.insert(TABLE_ROUTES, null, values);
+
+        db.close();
+        return id;
+    }
+*/
+    /**
+     * Adds a New Route to the Table
+     * Will set the pid to highestpid+1 and rid to 0
+     * @param path
+     * @return
+     */
+    public long addNewRoute(Route path) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        //get count of routes to set new pid
+
+        values.put(KEY_JSON, path.toString());
+        values.put(KEY_PID, path.getPID());
+        values.put(KEY_RID, 0);
+
+        long id = db.insert(TABLE_ROUTES, null, values);
+
+        db.close();
+        return id;
+    }
+
+    public long addRun(Route path) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(KEY_JSON, path.toString());
+        values.put(KEY_PID, path.getPID());
         values.put(KEY_RID, path.getRID());
 
         long id = db.insert(TABLE_ROUTES, null, values);

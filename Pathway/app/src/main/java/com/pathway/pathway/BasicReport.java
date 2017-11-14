30d2392791223b2cf2daa8e8777ac2abcafa08fa
Eric West <bactax10@gmail.com>
2017-11-13 20:01:46 -0500

    Added framework for report classes

--- Pathway/app/src/main/java/com/pathway/pathway/BasicReport.java
+++ Pathway/app/src/main/java/com/pathway/pathway/BasicReport.java
@@ -1,14 +1,81 @@
 package com.pathway.pathway;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 /**
  * Created by eewest on 11/13/17.
  */
 
-public class BasicReport {
+public class BasicReport extends JSONObject{
     //current run
-    ArrayList<double> speed_y = new ArrayList<double>();
-    ArrayList<int> time_x = new ArrayList<int>();
-    //best run
+    private ArrayList<Double> speed_y;
+    private ArrayList<Integer> time_x;
+    private double maxSpeed;
+    private double avgSpeed;
+    private int totalTimeSec;
+
+    public BasicReport(){
+        int routeTbl_ID;
+        setSpeed_y(new ArrayList<Double>());
+        setTime_x(new ArrayList<Integer>());
+        setMaxSpeed(0);
+        setAvgSpeed(0);
+        setTotalTimeSec(0);
+        routeTbl_ID = -1;
+
+        try {
+            this.put("routeTbl_ID",routeTbl_ID);
+            this.put("speed_y", new JSONArray(getSpeed_y().toArray()));
+            this.put("time_x", new JSONArray(getTime_x().toArray()));
+            this.put("maxSpeed", getMaxSpeed());
+            this.put("avgSpeed", getAvgSpeed());
+            this.put("totalTimeSec", getTotalTimeSec());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Double> getSpeed_y() {
+        return speed_y;
+    }
+
+    public void setSpeed_y(ArrayList<Double> speed_y) {
+        this.speed_y = speed_y;
+    }
+
+    public ArrayList<Integer> getTime_x() {
+        return time_x;
+    }
+
+    public void setTime_x(ArrayList<Integer> time_x) {
+        this.time_x = time_x;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public double getAvgSpeed() {
+        return avgSpeed;
+    }
+
+    public void setAvgSpeed(double avgSpeed) {
+        this.avgSpeed = avgSpeed;
+    }
+
+    public int getTotalTimeSec() {
+        return totalTimeSec;
+    }
+
+    public void setTotalTimeSec(int totalTimeSec) {
+        this.totalTimeSec = totalTimeSec;
+    }
 }
--- Pathway/app/src/main/java/com/pathway/pathway/UserReport.java
+++ Pathway/app/src/main/java/com/pathway/pathway/UserReport.java
@@ -1,16 +1,89 @@
 package com.pathway.pathway;
 
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by eewest on 11/13/17.
  */
 
-public class UserReport {
-    double totalDistance;
-    double totalTime;
-    int numRuns;
-    int numRoutes;
+public class UserReport extends JSONObject{
+    private double totalDistance;
+    private double totalTime;
+    private int numRuns;
+    private int numRoutes;
 
     public UserReport(){
+        setTotalDistance(0);
+        setTotalTime(0);
+        setNumRuns(0);
+        setNumRoutes(0);
+
+        try {
+            this.put("totalDistance", totalDistance);
+            this.put("totalTime", totalTime);
+            this.put("numRuns", numRuns);
+            this.put("numRoutes", numRoutes);
+        }catch(JSONException e){
+            Log.d("PathwayError", e.getMessage());
+        }
+    }
+
+
+    public double getTotalDistance() {
+        return totalDistance;
+    }
+
+    public void setTotalDistance(double totalDistance) {
+        try {
+            this.put("totalDistance", totalDistance);
+            this.totalDistance = totalDistance;
+        }catch(JSONException e) {
+            Log.d("PathwayError", e.getMessage());
+        }
+    }
+
+    public double getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(double totalTime) {
+        try {
+            this.put("totalTime", totalTime);
+            this.totalTime = totalTime;
+        }catch(JSONException e){
+            Log.d("PathwayError", e.getMessage());
+        }
+
+    }
+
+    public int getNumRuns() {
+        return numRuns;
+    }
+
+    public void setNumRuns(int numRuns) {
+        try {
+            this.put("numRuns", numRuns);
+            this.numRuns = numRuns;
+        }catch(JSONException e){
+            Log.d("PathwayError", e.getMessage());
+        }
+
+    }
+
+    public int getNumRoutes() {
+        return numRoutes;
+    }
+
+    public void setNumRoutes(int numRoutes) {
+        try{
+            this.put("numRoutes", numRoutes);
+            this.numRoutes = numRoutes;
+        }catch(JSONException e){
+            Log.d("PathwayError", e.getMessage());
+        }
 
     }
 }

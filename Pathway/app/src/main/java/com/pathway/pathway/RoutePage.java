ba79360db80103f4983679978e657124c29e6e46
grif145 <dkcregan@uncg.edu>
2017-11-14 22:16:56 -0500

    Finished the routes page
    This should when a user has a route saved to the local database, upon tapping the route tab diaplay the route name and any pertinant info about the route inside a card view inside a recycler view. as of right now nothing currently disaplays. Because there are no routes on the local database and to add one my versions location set button would have to work but it does not.

--- Pathway/app/src/main/java/com/pathway/pathway/RoutePage.java
+++ Pathway/app/src/main/java/com/pathway/pathway/RoutePage.java
@@ -5,7 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Daniel Cregan on 11/12/2017.
@@ -20,6 +23,20 @@ public class RoutePage extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.rv);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        final JSONObject jsonObject = new JSONObject();
+        ArrayList<Route> jsonStuff = new ArrayList<>();
+        DeviceDBHandler db = new DeviceDBHandler(getApplicationContext());
+        List<String> jsonString = db.getUserRoutes();
+        for (String element : jsonString){
+            try {
+                Route r = new Route(element);
+                jsonStuff.add(r);
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
         ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add("route1");
         arrayList.add("route2");
@@ -32,6 +49,6 @@ public class RoutePage extends AppCompatActivity {
         arrayList.add("route9");
         arrayList.add("route10");
 
-        recyclerView.setAdapter(new RecyclerAdapter(arrayList));
+        recyclerView.setAdapter(new RecyclerAdapter(jsonStuff));
     }
 }
